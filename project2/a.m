47047c3fbe48cb8bbe5b918aee728b8caa895d0e
william <williamjwking@gmail.com>
2016-06-29 22:38:27 +0800

    complete

--- project2/a.m
+++ project2/a.m
@@ -1,24 +1,68 @@
-t = -1:0.01:1;
-m = -1:0.01:1;
+wp = 3 * 2.0 / 15;
+ws = 5 * 2.0 / 15;
+wc = (wp + ws) / 2;
 
-for i = -100: 100
-    if (abs(i) <= 53)
-        m(i + 101) = 1;
-    else
-        m(i + 101) = 0;
-    end
-end
+alphaP = 1 - 10 ^ (0.3 / -20);
+alphaS = 10 ^ (40 / -20);
+
+[n, Wn, beta, ftype] = kaiserord([wp, ws], [1, 0], [alphaP, alphaS]);
+
+kaiser_win = kaiser(n+1, beta);
+hamm_win = hamming(n+1);
+hann_win = hanning(n+1);
+blackman_win = blackman(n+1);
+
+wins = [hamm_win, hann_win, blackman_win, kaiser_win]
+titles = {
+    'hamm impluse response',
+    'hann impluse response',
+    'blackman impluse response',
+    'kaiser impluse response'
+    };
+  
+titles2 = {
+    'hamm gain response',
+    'hann gain response',
+    'blackman gain response',
+    'kaiser gain response'
+    };
+
+M = n / 2;
+n = -M : M;
+hd = sin(wc * pi * n) ./ (pi * n);
+hd(find(n==0)) = wc;
 
 figure(1);
-plot(t,m);
-xlabel('w/pi sacled frequency');
-ylim([0,1.2]);
-ylabel('Magnitude');
+plot(n, hd, '-');
+title('ideal impulse response');
+xlabel('n');
+ylabel('hd');
 
-x = [-1, -0.67, -0.4, 0, 0.4, 0.67, 1];
-y = [0, 0, 1, 1, 1, 0, 0];
+for i = 1 : 4
+  figure(i + 1);
+  
+  subplot(1,2,1);
+  ht = hd .* wins(i);
+  plot(n, ht, '-');
+  title(titles(i));
+  xlabel('n');
+  ylabel('hd');
+  grid on;
 
-plot(x, y, '-r');
-xlabel('w/pi sacled frequency');
-ylim([0,1.2]);
-ylabel('Magnitude');
+  subplot(1,2,2);
+  [h, w] =freqz(ht, 1, 512);
+  w = w / pi;
+  h = 20 * log10(abs(h));
+  plot(w, h, '-');
+  title(titles2(i));
+  xlabel('pi(w/s)');
+  ylabel('gain(db)');
+  grid on;
+  
+end
+
+
+
+
+
+
--- project2/b.m
+++ project2/b.m
@@ -6,17 +6,48 @@ fcuts = [3.0/15*2, 5.0/15*2];
 mags = [1, 0];
 devs = [alphaP, alphaS];
 [n, Wn, beta, ftype] = kaiserord(fcuts, mags, devs);
-hh = fir1(n, Wn, ftype, kaiser(n+1, beta), 'noscale');
-freqz(hh);
 
-figure(2);
-hh = fir1(n, Wn, ftype, hamming(n+1), 'noscale');
-freqz(hh);
+kaiser_win = kaiser(n+1, beta);
+hamm_win = hamming(n+1);
+hann_win = hanning(n+1);
+blackman_win = blackman(n+1);
 
-figure(3);
-hh = fir1(n, Wn, ftype, hanning(n+1), 'noscale');
-freqz(hh);
+wins = [hamm_win, hann_win, blackman_win, kaiser_win]
+titles = {
+    'hamm impluse response',
+    'hann impluse response',
+    'blackman impluse response',
+    'kaiser impluse response'
+    };
+  
+titles2 = {
+    'hamm gain response',
+    'hann gain response',
+    'blackman gain response',
+    'kaiser gain response'
+    };
 
-figure(4);
-hh = fir1(n, Wn, ftype, blackman(n+1), 'noscale');
-freqz(hh);
+x = -n/2 : n/2;
+
+for i = 1 : 4
+  figure(i);
+  
+  subplot(1,2,1);
+  hh = fir1(n, Wn, ftype, kaiser(n+1, beta));
+  plot(x, hh, '-');
+  title(titles(i));
+  xlabel('n');
+  ylabel('hd')
+  grid on;
+  
+  subplot(1,2,2);
+  [h, w] =freqz(hh, 1, 512);
+  w = w / pi;
+  h = 20 * log10(abs(h));
+  plot(w, h, '-');
+  title(titles2(i));
+  xlabel('pi(w/s)');
+  ylabel('gain(db)');
+  grid on;
+
+end
