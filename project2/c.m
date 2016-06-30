61277edf4c6a37e7e43c433754c06c6ddfbdf869
william <williamjwking@gmail.com>
2016-06-30 11:22:04 +0800

    pic

--- project2/c.m
+++ project2/c.m
@@ -3,24 +3,27 @@
 [H, W] =zp2tf(Z, P, K);
 figure(1);
 [P, Q] = freqs(H, W);
-plot(Q * 15/(2*pi), abs(P));
+plot(Q * 15/(2*pi), 20 * log10(abs(P)));
 xlabel('frequency/HZ');
-ylabel('magnitude');
+ylabel('gain/db');
+figure(1);
+
 
 [N, Wn] = cheb2ord(3, 5, 0.3, 40, 's');
 [Z, P, K] = cheby2(N, 40, Wn, 's');
 [H, W] =zp2tf(Z, P, K);
 figure(2);
 [P, Q] = freqs(H, W);
-plot(Q * 15/(2*pi), abs(P));
+Q= Q * 15/(2*pi);
+plot(Q, 20 * log10(abs(P)));
 xlabel('frequency/HZ');
-ylabel('magnitude');
+ylabel('gain/db');
 
 [N, Wn] = ellipord(3, 5, 0.3, 40, 's');
 [Z, P, K] = ellip(N, 0.3, 40, Wn, 's');
 [H, W] =zp2tf(Z, P, K);
 figure(3);
 [P, Q] = freqs(H, W);
-plot(Q * 15/(2*pi), abs(P));
+plot(Q * 15/(2*pi), 20 * log(abs(P)));
 xlabel('frequency/HZ');
-ylabel('magnitude');
+ylabel('gain/db');
